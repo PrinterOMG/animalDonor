458b0764e57f47375be2912cc06b554f10236138
PrinterOMG <PrinterOMG@ya.ru>
2024-02-23 18:25:10 +0300

    Add patch for current user

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,11 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
-from api.dependencies import current_active_user_dep
-from api.schemas.users import UserRead
+from api.dependencies import current_active_user_dep, db_session_dep
+from api.schemas.users import UserRead, UserUpdate
 
 router = APIRouter(prefix='/users', tags=['Users'])
 
 
 @router.get('/me')
 async def get_me(current_active_user: current_active_user_dep) -> UserRead:
+    return current_active_user
+
+
+@router.patch('/me')
+async def update_me(
+        user_update: UserUpdate,
+        current_active_user: current_active_user_dep,
+        db_session: db_session_dep
+) -> UserRead:
+    update_data = user_update.model_dump(exclude_unset=True)
+
+    if not update_data:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='At least one field is required')
+
+    for field, value in update_data.items():
+        setattr(current_active_user, field, value)
+
+    await db_session.commit()
+
     return current_active_user
--- api/schemas/users.py
+++ api/schemas/users.py
@@ -1,12 +1,20 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, Field
 
 
 class UserBase(BaseModel):
     first_name: str | None
     second_name: str | None
-    email: str
-    is_active: bool
+
 
 
 class UserRead(UserBase):
     id: int
+    email: str
+    is_active: bool
+
+
+class UserUpdate(UserBase):
+    first_name: Annotated[str | None, Field(min_length=2)] = None
+    second_name: Annotated[str | None, Field(min_length=2)] = None
