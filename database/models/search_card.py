d36f499696a321ae6c16be77bffa2056a543f054
PrinterOMG <PrinterOMG@ya.ru>
2024-02-24 08:47:27 +0300

    Update search card model & fix migrations

--- database/models/search_card.py
+++ database/models/search_card.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 import sqlalchemy
 from sqlalchemy import ForeignKey
@@ -13,12 +13,12 @@ class SearchCard(Base):
     description: Mapped[str] = mapped_column(nullable=True)
     destination_point: Mapped[str] = mapped_column(nullable=True)
     blood_amount: Mapped[int] = mapped_column(nullable=True)
-    active_until: Mapped[datetime] = mapped_column(nullable=True)
-    is_active: Mapped[bool] = mapped_column(nullable=True)
-    created_at: Mapped[datetime] = mapped_column(nullable=True)
+    active_until: Mapped[date] = mapped_column(nullable=True)
+    is_active: Mapped[bool] = mapped_column(nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.utcnow)
 
-    author_id: Mapped[int] = mapped_column(sqlalchemy.BigInteger, ForeignKey('user.id'), nullable=True)
-    recipient_id: Mapped[int] = mapped_column(sqlalchemy.BigInteger, ForeignKey('pet.id'), nullable=True)
+    author_id: Mapped[int] = mapped_column(sqlalchemy.BigInteger, ForeignKey('user.id'), nullable=False)
+    recipient_id: Mapped[int] = mapped_column(sqlalchemy.BigInteger, ForeignKey('pet.id'), nullable=False)
 
     author: Mapped['User'] = relationship('User', backref='search_cards')
     recipient: Mapped['Pet'] = relationship('Pet', backref='search_cards')
